697e68f2b030601df27c5961d7e7933241d3d867
KrzysztofSarapuk <sarapuk2017@wp.pl>
2021-10-09 18:54:27 +0200

    pawn controller

---
+++ Assets/Scenes/scripts/movementController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void Move(Vector2Int direction)
+    {
+        transform.position += new Vector3(direction.x, 0, direction.y);
+        GetComponent<Animator>().Play("moveforward");
+    }
+}
---
+++ Assets/Scenes/scripts/playerInput.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("space pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            GetComponent<movementController>().Move(new Vector2Int(0, 1));
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            GetComponent<movementController>().Move(new Vector2Int(0, -1));
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GetComponent<movementController>().Move(new Vector2Int(1, 0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            GetComponent<movementController>().Move(new Vector2Int(-1, 0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+           
+        }
+    }
+}
